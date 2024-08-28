345192f6b4161db4a932b702074b34cd6588066c
Oksana <oksana.s.tsoy@gmail.com>
2024-08-28 14:00:11 +0400

    cicd pipeline added

--- src/test/java/com/otsoi/playtests/AppConfiguration.java
+++ src/test/java/com/otsoi/playtests/AppConfiguration.java
@@ -4,18 +4,27 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import com.otsoi.playtests.pages.SpringContext;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.SimpleThreadScope;
 
 @Configuration
+@PropertySource("classpath:test-config.yml")
 public class AppConfiguration {
+    @Value("${playwright.headless:true}")
+    @Autowired
+    private boolean headlessMode;
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @PostConstruct
     public void init() {
         SimpleThreadScope threadScope = new SimpleThreadScope();
@@ -23,14 +32,11 @@ public class AppConfiguration {
         beanFactory.registerScope("threadScope", threadScope);
     }
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @Bean("Secondary")
     @Scope("threadScope")
     public Page page() {
-        Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        var page =  browser.newPage();
+        Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(headlessMode));
+        var page = browser.newPage();
         System.out.println("CREATED" + page.toString());
         return page;
     }
